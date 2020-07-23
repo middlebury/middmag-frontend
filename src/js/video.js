128f5f6d8a4ad60e44adfc1a880de094f15c9e51
Zeb Pykosz <zeb.apy@gmail.com>
2020-07-23 09:15:55 -0400

    wrap up featured video on post

--- src/js/video.js
+++ src/js/video.js
@@ -9,20 +9,8 @@ function createLoadableVideo(elem) {
     iframe.allow = 'autoplay;fullscreen';
     iframe.allowFullscreen = true;
 
-    // iframe.style.opacity = '0';
-
     elem.innerHTML = '';
     elem.appendChild(iframe);
-    // elem.classList.add('video--loading');
-
-    // iframe.onload = () => {
-    //   elem.addEventListener('transitionend', () => {
-    //     elem.classList.remove('video--loading');
-    //     elem.classList.add('video--loaded');
-    //     iframe.style.opacity = '1';
-    //     console.log('done');
-    //   });
-    // };
   }
 
   link.addEventListener('click', handleClick);
