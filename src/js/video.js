363ae31fd80ae89c4dd89600ff985342f65e2dc4
Zeb Pykosz <zeb.apy@gmail.com>
2020-07-27 16:22:21 -0400

    make video play on spacebar press

--- src/js/video.js
+++ src/js/video.js
@@ -1,9 +1,9 @@
 function createLoadableVideo(elem) {
   const link = elem.querySelector('a');
 
-  function handleClick(event) {
-    event.preventDefault();
+  link.setAttribute('role', 'button');
 
+  function showVideo() {
     const iframe = document.createElement('iframe');
     iframe.src = link.href;
     iframe.allow = 'autoplay;fullscreen';
@@ -13,7 +13,20 @@ function createLoadableVideo(elem) {
     elem.appendChild(iframe);
   }
 
+  function handleClick(event) {
+    event.preventDefault();
+    showVideo();
+  }
+
+  function handleKeyPress(event) {
+    if (event.code === 'Space') {
+      event.preventDefault();
+      showVideo();
+    }
+  }
+
   link.addEventListener('click', handleClick);
+  link.addEventListener('keydown', handleKeyPress);
 }
 
 const videos = document.querySelectorAll('.js-video');
