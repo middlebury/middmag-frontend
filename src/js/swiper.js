3f7bb1bcb625fcae492b538f063fbcd97e9a42ff
Zeb Pykosz <zeb.apy@gmail.com>
2020-07-29 14:48:26 -0400

    add isNan polyfill for ie11

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,5 +1,13 @@
 import { Swiper, Navigation, A11y } from 'swiper';
 
+// ie11 polyfill
+// https://github.com/nolimits4web/swiper/issues/3698
+Number.isNaN =
+  Number.isNaN ||
+  function(value) {
+    return typeof value === 'number' && isNaN(value);
+  };
+
 Swiper.use([Navigation, A11y]);
 
 export default Swiper;
