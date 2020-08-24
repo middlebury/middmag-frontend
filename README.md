8a287b561915e02e4d6faceaeee70beedb500a46
Zeb Pykosz <zeb.apy@gmail.com>
2020-08-24 15:24:07 -0400

    add deploy script to copy built files to theme directory

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # middmag-frontend
 
 ## Requires
+
 - node 12.x
 
 ## Getting started
+
 - Clone repo
 - `cd middmag-frontend`
 - Install node packages `npm install`
 - Build templates/css/js `npm run build`
 - OR `npm start` to run build tasks + local server with livereload
+
+## Deploy
+
+Copy built files to a Drupal theme folder.
+
+```bash
+THEME_DIR=/path/to/theme npm run deploy
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,37 @@
+const gulp = require('gulp');
 const { createConfig } = require('@middlebury/gulp-config');
 
-module.exports = createConfig({
+const { THEME_DIR, IMAGES_DIR } = process.env;
+
+const cmds = createConfig({
   browserSyncOptions: {
     directory: true
+  },
+  // default config sets images as /img dir
+  images: {
+    src: './src/images/*.{jpg,png,svg}',
+    watch: './src/images/*.{jpg,png,svg}',
+    dest: './dist/images'
   }
 });
+
+const deploy = () => {
+  if (!THEME_DIR) {
+    throw new Error(`
+      Missing THEME_DIR environment variable. Example:
+
+      THEME_DIR=/path/to/theme npm run deploy
+    `);
+  }
+
+  return gulp
+    .src(['./dist/css/*', './dist/js/*', './dist/images/*'], {
+      base: './dist'
+    })
+    .pipe(gulp.dest(THEME_DIR));
+};
+
+module.exports = {
+  ...cmds,
+  deploy
+};
