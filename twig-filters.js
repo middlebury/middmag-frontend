352aecb2787d416d6be8deba3986eeed519eb631
poojagunturu96 <pgunturu@middlebury.edu>
2023-05-31 12:51:10 -0500

    Update packages, placeholder link for images

--- twig-filters.js
+++ twig-filters.js
@@ -1,7 +1,7 @@
 const buildSrcSet = sizes => {
   if (!sizes) return;
 
-  const prefix = 'https://placehold.it/';
+  const prefix = 'https://via.placeholder.com/';
 
   const last = sizes[sizes.length - 1];
   const src = prefix + last;
@@ -23,7 +23,7 @@ const buildSrcSet = sizes => {
 
 const getResponsiveImageStyles = img => {
   const { src, srcset, width } = buildSrcSet(img.srcset);
-
+  
   const sizes = img.sizes
     ? img.sizes
     : `(max-width: ${width}px) 100vw, ${width}px)`;
